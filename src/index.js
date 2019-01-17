acb67cebde5cf42db9155566983539aaccd5ced7
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-17 15:58:31 -0700

    configure geolocation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    position => console.log(position),
+    error => console.log(error)
+  ) //takes some time to determine where user location is
+
   return (
     <div>hello seasons</div>
   );
