894103b71450ef7ada97e8012f5fdbf5b3fb1157
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-17 15:51:34 -0700

    modify index.js and create SeasonContent comp

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>hello seasons</div>
+  );
+};
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
