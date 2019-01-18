7a46bc8636eab66b8678cfdf61db12ce031c8900
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-18 11:06:17 -0700

    initialize state

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   //This function is not required by React, it's a JS function
   //This function will automatically be called before anything else
-  constructor() {
+  constructor(props) {
+    super(props)
 
+    this.state = { lat: null } //default of null means 'i dont know what the latitude value is yet'
   }
 
   //React says I have to define render()!!
