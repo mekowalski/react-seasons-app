c6f05087f063e84f5072d8796c8de2060928a0a5
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-18 14:49:08 -0700

    console log works correctly for my lat, month and season!!!

--- src/SeasonContent.js
+++ src/SeasonContent.js
@@ -4,13 +4,17 @@ import React from 'react';
 //return the string either Summer or Winter
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) { //this will only handle summer months
-    return lat > 0 ? 'Summer' : 'Winter' //if it is then it must be the Northern Hemisphere
+    return lat > 0 ? 'Summer' : 'Winter'; //if it is then it must be the Northern Hemisphere
   }//if lat is above 0 then it will return summer, if below 0 it wll return winter
+  else {
+    return lat > 0 ? 'Winter' : 'Summer';
+  }
 }
 
 const SeasonContent = props => {
   //make sure to be able to call getSeason function
   const season = getSeason(props.lat, new Date().getMonth());
+  console.log(season)
 
   return (
     <div>Season Content</div>
