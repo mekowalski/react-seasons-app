aaf021008b7dc702522adc5b138b09c47cf4967b
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-21 09:57:16 -0700

    create and add Loading Comp file

---
+++ src/Loading.js
@@ -0,0 +1,13 @@
+import React fron 'react';
+
+const Loading = () => {
+  return (
+    <div className='ui active dimmer'>
+      <div className='ui big text loader'>
+        Loading...
+      </div>
+    </div>
+  );
+};
+
+export default Loading
